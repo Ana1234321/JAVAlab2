892a99ec5f75ecd3e50b9fb617b3a1b7dc9d2526
Naya <qwwsoul@gmail.com>
2019-12-27 15:00:19 +0300

    imports+vars

---
+++ src/MainFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.lang.StrictMath;
+import java.text.AttributedCharacterIterator;
+import static java.lang.Math.*;
+
+public class MainFrame extends JFrame {
+    private static final int width = 1300;
+    private static final int height = 500;
+    private JTextField result_field;
+    private JTextField x_field;
+    private JTextField y_field;
+    private JTextField z_field;
+    private JLabel image;
+    private ButtonGroup radioButtons = new ButtonGroup();
+    private Box formula_type = Box.createHorizontalBox();
+    private ButtonGroup radioMemoryButtons = new ButtonGroup();
+    private Box hBoxMemoryType = Box.createHorizontalBox();
+    private JTextField memoryTextField;
+    int formula_number = 1;
+    private int memoryId = 1;
+
+    private Double mem1 = (double) 0;
+    private Double mem2 = (double) 0;
+    private Double mem3 = (double) 0;
